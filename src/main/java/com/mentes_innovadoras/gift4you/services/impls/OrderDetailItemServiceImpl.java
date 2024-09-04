de8788348f234ed142d8c17ae56ae75ea71ddc73
hoangbaohuy <144062987+hoangbaohuy@users.noreply.github.com>
2024-09-05 00:30:38 +0700

    update order detail item

--- src/main/java/com/mentes_innovadoras/gift4you/services/impls/OrderDetailItemServiceImpl.java
+++ src/main/java/com/mentes_innovadoras/gift4you/services/impls/OrderDetailItemServiceImpl.java
@@ -1,5 +1,7 @@
 package com.mentes_innovadoras.gift4you.services.impls;
 
+import com.mentes_innovadoras.gift4you.entity.InventoryItem;
+import com.mentes_innovadoras.gift4you.entity.OrderDetail;
 import com.mentes_innovadoras.gift4you.entity.OrderDetailItem;
 import com.mentes_innovadoras.gift4you.exception.common.InvalidParamException;
 import com.mentes_innovadoras.gift4you.exception.core.ArchitectureException;
@@ -7,7 +9,9 @@ import com.mentes_innovadoras.gift4you.exception.account.UserNotFoundException;
 import com.mentes_innovadoras.gift4you.mapper.OrderDetailItemMapper;
 import com.mentes_innovadoras.gift4you.payload.reponse.OrderDetailItemResponse;
 import com.mentes_innovadoras.gift4you.payload.request.OrderDetailItemRequest;
+import com.mentes_innovadoras.gift4you.repository.InventoryItemRepository;
 import com.mentes_innovadoras.gift4you.repository.OrderDetailItemRepository;
+import com.mentes_innovadoras.gift4you.repository.OrderDetailRepository;
 import com.mentes_innovadoras.gift4you.services.interfaces.OrderDetailItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -22,6 +26,9 @@ import java.util.UUID;
 public class OrderDetailItemServiceImpl implements OrderDetailItemService {
     private final OrderDetailItemRepository orderDetailItemRepository;
     private final OrderDetailItemMapper orderDetailItemMapper;
+    private final OrderDetailRepository orderDetailRepository;
+    private final InventoryItemRepository inventoryItemRepository;
+
     @Override
     public Page<OrderDetailItemResponse> getOrderDetailItems(Pageable pageable) {
         return orderDetailItemRepository.findAll(pageable).map(orderDetailItemMapper::toOrderDetailItemResponse);
@@ -38,12 +45,19 @@ public class OrderDetailItemServiceImpl implements OrderDetailItemService {
     @Override
     public OrderDetailItemResponse createOrderDetailItem(OrderDetailItemRequest orderDetailItemRequest) throws ArchitectureException {
         OrderDetailItem newOrderDetailItem = orderDetailItemMapper.toOrderDetailItemEntity(orderDetailItemRequest);
+        OrderDetail orderDetail = orderDetailRepository.findById(orderDetailItemRequest.getOrderDetail().getId()).orElse(null);
+        InventoryItem inventoryItem = inventoryItemRepository.findById(orderDetailItemRequest.getInventoryItem().getId()).orElse(null);
         newOrderDetailItem.setId(UUID.randomUUID());
         newOrderDetailItem.setQuantity(orderDetailItemRequest.getQuantity());
+        newOrderDetailItem.setOrderDetail(orderDetail);
+        newOrderDetailItem.setInventoryItem(inventoryItem);
         return orderDetailItemMapper.toOrderDetailItemResponse(orderDetailItemRepository.save(newOrderDetailItem));    }
 
     @Override
     public OrderDetailItemResponse updateOrderDetailItem(UUID id, OrderDetailItemRequest orderDetailItemRequest) throws ArchitectureException {
-        return null;
+        OrderDetailItem orderDetailItem = orderDetailItemRepository.findById(id).orElse(null);
+        if (orderDetailItem == null) throw new UserNotFoundException();
+        orderDetailItem.setQuantity(orderDetailItemRequest.getQuantity());
+        return orderDetailItemMapper.toOrderDetailItemResponse(orderDetailItemRepository.save(orderDetailItem));
     }
 }
