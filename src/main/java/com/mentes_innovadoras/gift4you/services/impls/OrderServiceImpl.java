cf069aa104ff09b2c088d9c2898bf44a1d520948
hoangbaohuy <144062987+hoangbaohuy@users.noreply.github.com>
2024-09-05 01:32:23 +0700

    update order

--- src/main/java/com/mentes_innovadoras/gift4you/services/impls/OrderServiceImpl.java
+++ src/main/java/com/mentes_innovadoras/gift4you/services/impls/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mentes_innovadoras.gift4you.services.impls;
 
+import com.mentes_innovadoras.gift4you.entity.Account;
 import com.mentes_innovadoras.gift4you.entity.Order;
 import com.mentes_innovadoras.gift4you.exception.common.InvalidParamException;
 import com.mentes_innovadoras.gift4you.exception.core.ArchitectureException;
@@ -7,6 +8,7 @@ import com.mentes_innovadoras.gift4you.exception.account.UserNotFoundException;
 import com.mentes_innovadoras.gift4you.mapper.OrderMapper;
 import com.mentes_innovadoras.gift4you.payload.reponse.OrderResponse;
 import com.mentes_innovadoras.gift4you.payload.request.OrderRequest;
+import com.mentes_innovadoras.gift4you.repository.AccountRepository;
 import com.mentes_innovadoras.gift4you.repository.OrderRepository;
 import com.mentes_innovadoras.gift4you.services.interfaces.OrderService;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,7 @@ import java.util.UUID;
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
+    private final AccountRepository accountRepository;
     @Override
     public Page<OrderResponse> getOrders(Pageable pageable) {
         return orderRepository.findAll(pageable).map(orderMapper::toOrderResponse);
@@ -40,6 +43,7 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public OrderResponse createOrder(OrderRequest orderRequest) throws ArchitectureException {
         Order newOrder = orderMapper.toOrderEntity(orderRequest);
+        Account account = accountRepository.findById(orderRequest.getAccount().getId()).orElse(null);
         newOrder.setId(UUID.randomUUID());
         newOrder.setCreateAt(new Date());
         newOrder.setUpdateAt(new Date());
@@ -48,11 +52,21 @@ public class OrderServiceImpl implements OrderService {
         newOrder.setStatus(orderRequest.getStatus());
         newOrder.setAddress(orderRequest.getAddress());
         newOrder.setTotalPrice(orderRequest.getTotalPrice());
+        newOrder.setAccount(account);
         return orderMapper.toOrderResponse(orderRepository.save(newOrder));
     }
 
     @Override
     public OrderResponse updateOrder(UUID id, OrderRequest orderRequest) throws ArchitectureException {
-        return null;
+        Order order = orderRepository.findById(id).orElse(null);
+        if (order == null) throw new UserNotFoundException();
+        order.setCreateAt(new Date());
+        order.setUpdateAt(new Date());
+        order.setDescription(orderRequest.getDescription());
+        order.setPhoneNumber(orderRequest.getPhoneNumber());
+        order.setStatus(orderRequest.getStatus());
+        order.setAddress(orderRequest.getAddress());
+        order.setTotalPrice(orderRequest.getTotalPrice());
+        return orderMapper.toOrderResponse(orderRepository.save(order));
     }
 }
